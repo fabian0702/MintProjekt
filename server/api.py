76969c2d32d99ad1b40d82d2280105da518e0c85
fabian0702 <example@example.com>
2023-12-11 13:41:09 +0100

    Added response endpoints

--- server/api.py
+++ server/api.py
@@ -8,7 +8,7 @@ api = FastAPI()
 
 results:dict[str, list[int]] = {}
 
-@api.post('/register_device/')
+@api.post('/api/register_device/')
 def add_device(location:str=''):
     results.update({location: []})
     payload = {'device':len(results), 'device_location':location}
@@ -16,7 +16,7 @@ def add_device(location:str=''):
     print(f'generated token with payload: {payload}')
     return {'token':token}
 
-@api.post('/submit_result/')
+@api.post('/api/submit_result/')
 def submit_result(result:Annotated[int, Body(embed=True)], token:Annotated[str, Body(embed=True)]):
     payload = jwt.decode(token, secret, algorithms=['HS256', ])
     location = payload['device_location']
@@ -27,6 +27,17 @@ def submit_result(result:Annotated[int, Body(embed=True)], token:Annotated[str, 
     print(results, filteredResults())
     return ''
 
+@api.get('/api/list_devices/')
+def list_devices():
+    print(results)
+    return list(results.keys())
+
+@api.post('/api/get_location_info/')
+def get_location_info(location:str):
+    if not location in results:
+        raise HTTPException(status_code=404, detail='The specified device was not found')
+    return filteredResults()[location]
+
 def resultsFilter(data:list[int], filterConstant:float=0.2):
     if not len(data):
         return 0
@@ -34,6 +45,8 @@ def resultsFilter(data:list[int], filterConstant:float=0.2):
     for d in data[1:]:
         filterValue = (1-filterConstant)*filterValue + filterConstant * d
     return filterValue
+
+
 
 def filteredResults():
     return {x:resultsFilter(results[x]) for x in results}
